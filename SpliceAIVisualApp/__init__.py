7cf49e1b857e8dd4024a7118009c5bb8bfcda11a
beboche <david.baux@inserm.fr>
2022-04-08 14:32:06 +0200

    cache is working properly

--- SpliceAIVisualApp/__init__.py
+++ SpliceAIVisualApp/__init__.py
@@ -1,15 +1,8 @@
 import os
 from flask import Flask
-# from flask_cors import CORS
-# https://flask-wtf.readthedocs.io/en/stable/csrf.html
-# from flask_wtf.csrf import CSRFProtect
 from flask_restful import Api
 from SpliceAIVisualApp.route.spliceai import SpliceAi
 from SpliceAIVisualApp.route.hello import Hello
-# from flask_caching import Cache
-
-
-# csrf = CSRFProtect()
 
 
 def create_app(test_config=None):
@@ -21,8 +14,6 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-    # csrf.init_app(app)
-    # cache = Cache(app)
     api = Api(app)
     api.add_resource(SpliceAi, "/spliceai")
     api.add_resource(Hello, "/hello")
--- SpliceAIVisualApp/route/spliceai.py
+++ SpliceAIVisualApp/route/spliceai.py
@@ -2,29 +2,41 @@
 import os
 import re
 import subprocess
+import json
 # flask imports
 from flask_restful import Resource
 from flask import request, jsonify, make_response, current_app
-from flask_caching import Cache
-
-# def getParentDirectory(path, levels=1):
-#     # from https://www.geeksforgeeks.org/get-parent-of-current-directory-using-python
-#     common = path
-#     # Using for loop for getting
-#     # starting point required for
-#     # os.path.relpath()
-#     for i in range(1, levels + 1):
-#         # Starting point
-#         common = os.path.dirname(common)
-#     # Parent directory upto specified level
-#     return os.path.relpath(path, common)
+import redis
 
 
 def getAppRootDirectory():
     return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
+def jsonify_spliceai(seq, spliacei_results):
+    # from spliceai string result and input sequence build a dict
+    # format: {'1': ('[ATCG]', 'spliceai_score'), ...}
+    # returns a json from this dict
+    # [list(t) for t in zip((input['wt_seq']), re.split(' ', results[1].replace(r']', '')))]
+    json_dict = {}
+    i = 1
+    for t in list(zip(seq, re.split(' ', spliacei_results.replace(r']', '')))):
+        json_dict[i] = t
+        i += 1
+    return json.dumps(json_dict), json_dict
+
+
+def get_data_form_cache(key, r):
+    # get data from redis cache and returns as json
+    data = r.get(key)
+    if data:
+        return json.loads(data)
+    else:
+        return None
+
+
 def return_json(message, spliecai_return_code=1, http_code=200, result=None):
+    # prepare flask response
     return make_response(jsonify({
         'spliecai_return_code': spliecai_return_code,
         'result': result,
@@ -34,20 +46,25 @@ def return_json(message, spliecai_return_code=1, http_code=200, result=None):
 
 class SpliceAi(Resource):
     def get(self):
+        r = redis.Redis(
+            host=current_app.config['CACHE_REDIS_HOST'],
+            port=current_app.config['CACHE_REDIS_PORT'],
+            socket_timeout=current_app.config['CACHE_DEFAULT_TIMEOUT'],
+            db=0
+        )
         input = request.get_json()
         # print(input)
         context = '10000'
-        cache = Cache(current_app)
         wt_acceptor = wt_donor = mt_acceptor = mt_donor = None
         if 'context' in input and \
                 re.search(r'^\d+$', str(input['context'])):
             context = str(input['context'])
         if 'wt_seq' in input and \
                 'mt_seq' in input:
-            wt_acceptor = cache.get('{}{}_acceptor'.format(current_app.config['CACHE_KEY_PREFIX'], input['wt_seq']))
-            wt_donor = cache.get('{}{}_donor'.format(current_app.config['CACHE_KEY_PREFIX'], input['wt_seq']))
-            mt_acceptor = cache.get('{}{}_acceptor'.format(current_app.config['CACHE_KEY_PREFIX'], input['mt_seq']))
-            mt_donor = cache.get('{}{}_donor'.format(current_app.config['CACHE_KEY_PREFIX'], input['mt_seq']))
+            wt_acceptor = get_data_form_cache('{}_acceptor'.format(input['wt_seq']), r)
+            wt_donor = get_data_form_cache('{}_donor'.format(input['wt_seq']), r)
+            mt_acceptor = get_data_form_cache('{}_acceptor'.format(input['mt_seq']), r)
+            mt_donor = get_data_form_cache('{}_donor'.format(input['mt_seq']), r)
             if (wt_acceptor and
                     wt_donor and
                     mt_acceptor and
@@ -86,8 +103,8 @@ class SpliceAi(Resource):
             else:
                 return return_json('Bad wt or mt sequences submitted')
         elif 'mt_seq' in input:
-            mt_acceptor = cache.get('{}{}_acceptor'.format(current_app.config['CACHE_KEY_PREFIX'], input['mt_seq']))
-            mt_donor = cache.get('{}{}_donor'.format(current_app.config['CACHE_KEY_PREFIX'], input['mt_seq']))
+            mt_acceptor = get_data_form_cache('{}_acceptor'.format(input['mt_seq']), r)
+            mt_donor = get_data_form_cache('{}_donor'.format(input['mt_seq']), r)
             if (mt_acceptor and
                     mt_donor):
                 # return values from cache
@@ -127,28 +144,26 @@ class SpliceAi(Resource):
         if result.returncode == 0:
             # success
             results = re.split(r'\[', str(result.stdout, 'utf-8').replace('\n', ''))
-            # print(results)
-            # print(results[1])
-            if not re.search(r']', results[1]):
+            print(results)
+            if not re.search(r'no_wt', results[1]):
                 # print(results)
                 # wt and mutant results
-                wt_acceptor = [list(t) for t in zip((input['wt_seq']), re.split(' ', results[1].replace(r']', '')))]
-                wt_donor = [list(t) for t in zip((input['wt_seq']), re.split(' ', results[2].replace(r']', '')))]
-                mt_acceptor = [list(t) for t in zip((input['mt_seq']), re.split(' ', results[3].replace(r']', '')))]
-                mt_donor = [list(t) for t in zip((input['mt_seq']), re.split(' ', results[4].replace(r']', '')))]
+                wt_acceptor_redis, wt_acceptor = jsonify_spliceai(input['wt_seq'], results[1])
+                wt_donor_redis, wt_donor = jsonify_spliceai(input['wt_seq'], results[2])
+                mt_acceptor_redis, mt_acceptor = jsonify_spliceai(input['mt_seq'], results[3])
+                mt_donor_redis, mt_donor = jsonify_spliceai(input['mt_seq'], results[4])
                 # populate redis
-                cache.set('{}_acceptor'.format(input['wt_seq']), wt_acceptor)
-                cache.set('{}_donor'.format(input['wt_seq']), wt_donor)
-                cache.set('{}_acceptor'.format(input['mt_seq']), mt_acceptor)
-                cache.set('{}_donor'.format(input['mt_seq']), mt_donor)
+                r.set('{}_acceptor'.format(input['wt_seq']), wt_acceptor_redis)
+                r.set('{}_donor'.format(input['wt_seq']), wt_donor_redis)
+                r.set('{}_acceptor'.format(input['mt_seq']), mt_acceptor_redis)
+                r.set('{}_donor'.format(input['mt_seq']), mt_donor_redis)
             else:
                 # mutant only
-                mt_acceptor = [list(t) for t in zip((input['mt_seq']), re.split(' ', results[3].replace(r']', '')))]
-                mt_donor = [list(t) for t in zip((input['mt_seq']), re.split(' ', results[4].replace(r']', '')))]
+                mt_acceptor_redis, mt_acceptor = jsonify_spliceai(input['mt_seq'], results[3])
+                mt_donor_redis, mt_donor = jsonify_spliceai(input['mt_seq'], results[4])
                 # populate redis
-                print(result.stdout)
-                cache.set('{}_acceptor'.format(input['mt_seq']), mt_acceptor)
-                cache.set('{}_donor'.format(input['mt_seq']), mt_donor)
+                r.set('{}_acceptor'.format(input['mt_seq']), mt_acceptor_redis)
+                r.set('{}_donor'.format(input['mt_seq']), mt_donor_redis)
             return return_json(
                 None,
                 result.returncode,
