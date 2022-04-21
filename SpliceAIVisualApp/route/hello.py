7349ddaa95febadffa8df504e339f9e618d652fe
beboche <david.baux@inserm.fr>
2022-04-21 10:58:44 +0200

    rename folder

--- SpliceAIVisualApp/route/hello.py
+++
@@ -1,9 +0,0 @@
-from flask_restful import Resource
-from flask import jsonify, make_response
-
-
-class Hello(Resource):
-    def get(self):
-        return make_response(jsonify({
-                'spliceaivisual_status': 'running'
-            }), 200)
